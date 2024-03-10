63b9a08fa972523cbdba9afaa47b323a6f3ece8d
Abigail Pipkin <abigailpipkinttu@gmail.com>
2024-03-10 12:42:23 -0500

    created tables and committed them to db

--- createtables.py
+++ createtables.py
@@ -5,3 +5,36 @@ def tableCreation():
     # connecting to db
     conn = sqlite3.connect('QuizBowl.db')
     cr = conn.cursor()
+
+    # creating tables
+    cr.execute(f'''CREATE TABLE IF NOT EXISTS Business_Applications_Development (
+                                id INTEGER PRIMARY KEY,
+                                question TEXT,
+                                answer TEXT)
+                                ''')
+    cr.execute(f'''CREATE TABLE IF NOT EXISTS Behavioral_Economics (
+                                id INTEGER PRIMARY KEY,
+                                question TEXT,
+                                answer TEXT)
+                                ''')
+    cr.execute(f'''CREATE TABLE IF NOT EXISTS Economics_Workshop (
+                                id INTEGER PRIMARY KEY,
+                                question TEXT,
+                                answer TEXT)
+                                ''')
+    cr.execute(f'''CREATE TABLE IF NOT EXISTS Risk_Management_And_Insurance (
+                                id INTEGER PRIMARY KEY,
+                                question TEXT,
+                                answer TEXT)
+                                ''')
+    cr.execute(f'''CREATE TABLE IF NOT EXISTS Calculus_II (
+                                id INTEGER PRIMARY KEY,
+                                question TEXT,
+                                answer TEXT)
+                                ''')
+    # commit changes and close the connection
+    conn.commit()
+    conn.close()
+
+# call the function to create tables
+tableCreation()
